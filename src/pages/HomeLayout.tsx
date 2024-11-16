762fd8d4cdb1d87ae2284a7ed50e8ea2a3f36d04
victor <oguntayovictor65@gmail.com>
2024-11-16 20:34:48 +0100

    add header component

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,11 +1,12 @@
+import { Header } from "@/components/global"
 import { Outlet } from "react-router-dom"
 
 const HomeLayout = () => {
   return (
     <section>
-      <header></header>
+     <Header/>
       <nav></nav>
-      <main>
+      <main className="align-element py-20">
         <Outlet/>
       </main>
     </section>
