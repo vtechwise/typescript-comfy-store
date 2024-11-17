fa41bc0ae0149ea03836abf8b8a74f63dd854481
victor <oguntayovictor65@gmail.com>
2024-11-17 20:46:15 +0100

    work on about ui and start wokring on landing page ui

--- src/components/global/NavLinks.tsx
+++ src/components/global/NavLinks.tsx
@@ -4,7 +4,6 @@ import { NavLink } from "react-router-dom";
 const NavLinks = () => {
   return (
     <div className="hidden lg:flex justify-center items-center gap-x-4 ">
-      <h2 className="bg-testing">hello world</h2>
       {links.map((link) => {
         return (
           <NavLink
